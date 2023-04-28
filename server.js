fe33088710301a337d05e7e0d755a339e7dad40f
Eliot Johnson <eliotmjohnson@gmail.com>
2023-04-28 12:21:08 -0500

    Fixed Functionality

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const bots = require("./src/botsData");
 const { shuffle } = require("./src/shuffle");
 const path = require("path");
@@ -15,6 +16,7 @@ const playerRecord = {
 };
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
